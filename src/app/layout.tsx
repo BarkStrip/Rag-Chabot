83ca75306ea7230d5b362431f1092354ea00e123
Barek Stripling <barekstripling@gmail.com>
2025-08-07 12:02:31 -0400

    Improved layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full bg-gray-100">
+    <html lang="en" className="h-full">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-full bg-gray-700`}
       >
